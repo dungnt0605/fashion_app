e663684ed1d90409bd49fe1fc3c6eb6c63b75aba
dungnt0605 <sdvndung051230@gmail.com>
2024-04-03 12:47:42 +0700

    fead : model User

--- src/models/user.js
+++ src/models/user.js
@@ -1,8 +1,23 @@
 import { Schema } from "mongoose";
 
 const UserSchema = new Schema({
-    username : {
-        type : String,
-        minLength
-    }
-}) 
+  username: {
+    type: String,
+    minlength: 3,
+    required: true,
+    trim: true,
+    lowercase: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: true,
+    lowercase: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
+});
